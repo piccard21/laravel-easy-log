91544bb96a18c1721790b7558c6995b4e618c8bb
Piccard <piccard@web.de>
2017-08-12 15:13:49 +0200

    everything in config set to FALSE

--- src/config/laravel-easy-log.php
+++ src/config/laravel-easy-log.php
@@ -2,11 +2,11 @@
 
 return [
 	/*
-	  |--------------------------------------------------------------------------
-	  | DB-Connection
-	  | if you wanna use a seperate connection set 'use_default_connection' to false & fill out the below variables
-	  |--------------------------------------------------------------------------
-	 */
+		  |--------------------------------------------------------------------------
+		  | DB-Connection
+		  | if you wanna use a seperate connection set 'use_default_connection' to FALSE & fill out the below variables
+		  |--------------------------------------------------------------------------
+	*/
 	'db' => [
 		'logging_enabled' => FALSE,
 		'bubble' => TRUE,
@@ -18,48 +18,49 @@ return [
 		'password' => 'DBPASSWORD',
 		'port' => '3306',
 		/*
-		  |--------------------------------------------------------------------------
-		  | Table
-		  |--------------------------------------------------------------------------
-		  | Define here the name of the table in which should be logged. Also you can specify here some extra columns.
-		  | These are the same like you state in the Monolog-context, e.g. you can name here your app.
-		  | Hint: When you change the columns, you have to restart the server.
-		  | i.e.:
-		  |
-		  | Log::debug("CONTEXT log in DEBUG", array('col1' => 'Hi, I am a context log.'));
-		  | Log::debug("CONTEXT log in DEBUG", array('col2' => 'Hi, I am a context log.'));
-		  |
-		  | Log::debug("CONTEXT log in DEBUG", array(
-		  |     'col1' => 'Whatever, I am a context log in col1.',
-		  |     'col2' => 'Whatever, I am a context log in col2.'
-		  | ));
-		 */
+			  |--------------------------------------------------------------------------
+			  | Table
+			  |--------------------------------------------------------------------------
+			  | Define here the name of the table in which should be logged. Also you can specify here some extra columns.
+			  | These are the same like you state in the Monolog-context, e.g. you can name additional information you don't wanna integrate into message.
+			  | Hint: When you change the columns, you have to restart the server.
+			  | i.e.:
+			  |
+			  | Log::debug("CONTEXT log in DEBUG");
+			  | Log::info("CONTEXT log in DEBUG", array('col1' => 'Hi, I am a context log.'));
+			  | Log::alert("CONTEXT log in DEBUG", array('col2' => 'Hi, I am a context log.'));
+			  |
+			  | Log::debug("CONTEXT log in DEBUG", array(
+			  |     'col1' => 'Whatever, I am a context log in col1.',
+			  |     'col2' => 'Whatever, I am a context log in col2.'
+			  | ));
+		*/
 		'app' => env('APP_NAME', 'Laravel'),
-		'table' => 'lel',
+		'table' => 'logs',
 		'columns' => ['col1', 'col2'],
 		'view' => [
 			'big' => TRUE,
 			'message_column_width' => '10%',
 			'message_max_length' => '250',
-			'result-number' => 50
-		]
+			'result-number' => 50,
+		],
 	],
 	/*
-	  |--------------------------------------------------------------------------
-	  | FILES
-	  |--------------------------------------------------------------------------
-	  | log_levels:
-	  |   for each log-level inside the array you name, an extra file is used
-	  |   levels: debug, info, notice, warning, error, critical, alert, emergency
-	  |
-	  | output_format:
-	  |   context - you can add the subcontext to the context variable, e.g.:
-	  |
-	  |   "[%datetime%] %channel%.%level_name%: %message% \t%context.message% \t[%extra.server%]\n"
-	  |   Log::debug("CONTEXT log in DEBUG", array('message' => 'Hi, I am a context log.'));
-	 */
+		  |--------------------------------------------------------------------------
+		  | FILES
+		  |--------------------------------------------------------------------------
+		  | log_levels:
+		  |   for each log-level inside the array you name, an extra file is used
+		  |   levels: debug, info, notice, warning, error, critical, alert, emergency
+		  |
+		  | output_format:
+		  |   context - you can add the subcontext to the context variable, e.g.:
+		  |
+		  |   "[%datetime%] %channel%.%level_name%: %message% \t%context.message% \t[%extra.server%]\n"
+		  |   Log::debug("CONTEXT log in DEBUG", array('message' => 'Hi, I am a context log.'));
+	*/
 	'file' => [
-		'logging_enabled' => TRUE,
+		'logging_enabled' => FALSE,
 		'bubble' => TRUE,
 		'log_levels' => ['debug', 'info', 'error'],
 		'date_format' => 'd.m.Y, G:i:s',
@@ -72,15 +73,15 @@ return [
 		// when app.log is set to 'syslog'
 		'syslog' => [
 			'ident' => env('APP_NAME', 'Laravel'),
-			'facility' => 'local6'
-		]
+			'facility' => 'local6',
+		],
 	],
 	/*
-	  |--------------------------------------------------------------------------
-	  | STD
-	  |--------------------------------------------------------------------------
-	  | if you like you can log to STDOUT & STDERR
-	 */
+		  |--------------------------------------------------------------------------
+		  | STD
+		  |--------------------------------------------------------------------------
+		  | if you like you can log to STDOUT & STDERR
+	*/
 	'std' => [
 		'out' => [
 			'logging_enabled' => FALSE,
@@ -98,11 +99,11 @@ return [
 		],
 	],
 	/*
-	  |--------------------------------------------------------------------------
-	  | MAIL
-	  |--------------------------------------------------------------------------
-	  | send an email when an error-level is reached. Don't forget to fill out 'to', 'from' & the 'subject'
-	 */
+		  |--------------------------------------------------------------------------
+		  | MAIL
+		  |--------------------------------------------------------------------------
+		  | send an email when an error-level is reached. Don't forget to fill out 'to', 'from' & the 'subject'
+	*/
 	'mail' => [
 		'logging_enabled' => FALSE,
 		'bubble' => true,
@@ -113,5 +114,5 @@ return [
 		'maxColumnWidth' => 70, // The maximum column width that the message lines will have
 		'date_format' => 'd.m.Y, G:i:s',
 		'output_format' => "[%datetime%] %channel%.%level_name%: %message% \t%context% \t[%extra.server%]\n",
-	]
+	],
 ];
